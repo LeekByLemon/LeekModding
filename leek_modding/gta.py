0be784665144922cc2014b1526a62fb5fe73ec71
Hannele Ruiz <justlemoncl@gmail.com>
2023-01-18 13:47:58 -0300

    Added support for fetching natives

--- leek_modding/gta.py
+++ leek_modding/gta.py
@@ -1,5 +1,16 @@
+import logging
+
+from aiohttp import ClientResponseError
 from discord import Cog
 from leek import LeekBot
+
+LOGGER = logging.getLogger("leek_modding")
+NATIVE_LINKS = {
+    "gtav": "https://raw.githubusercontent.com/alloc8or/gta5-nativedb-data/master/natives.json",
+    "rdr3": "https://raw.githubusercontent.com/alloc8or/rdr3-nativedb-data/master/natives.json",
+    "fivem": "https://runtime.fivem.net/doc/natives_cfx.json"
+}
+NATIVES = []
 
 
 class GrandTheftAuto(Cog):
@@ -8,3 +19,24 @@ class GrandTheftAuto(Cog):
     """
     def __init__(self, bot: LeekBot):
         self.bot: LeekBot = bot
+
+    @Cog.listener()
+    async def on_connect(self):
+        for game, url in NATIVE_LINKS.items():
+            try:
+                async with await self.bot.get(url) as resp:
+                    resp.raise_for_status()
+                    json: dict[str, dict[str, dict[str, str]]] = await resp.json()
+
+                    for namespace, natives in json.items():
+                        for n_hash, n_data in natives.items():
+                            native = {
+                                "namespace": namespace,
+                                "hash": n_hash,
+                                **n_data
+                            }
+                            NATIVES.append(native)
+            except ClientResponseError as e:
+                LOGGER.error(f"Can't request {url}: Code {e.status}")
+            except BaseException:
+                LOGGER.exception(f"Unable to get {game} natives from {url}")
